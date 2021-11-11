e993de567c2de6f384a8d50c9a83330ce25000df
abdullah-saadeddin <46649606+abdullah-saadeddin@users.noreply.github.com>
2021-11-11 20:36:01 +0200

    add database connections

--- FrontendServer/app.js
+++ FrontendServer/app.js
@@ -1,9 +1,18 @@
 const express = require('express');
+const path = require("path")
 const cors = require('cors');
 const app = express() ;
 require('dotenv').config()
 app.use(cors());
 app.use(express.json());
+
+const knex = require("knex")({
+    client: "mysql",
+    connection: {
+        filename: path.join(__dirname, './items.db')
+    },
+    useNullAsDefault: true
+});
 
 
 app.get("/",(req,res)=>{

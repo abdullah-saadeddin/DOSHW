c20bb299efb9ec543c7b933a09b52b110ff00a12
mohamedShamoot <mohammad99sham@gmail.com>
2021-11-11 21:49:37 +0200

    add purshuse product

--- OrdersServer/app.js
+++ OrdersServer/app.js
@@ -7,7 +7,7 @@ app.use(cors());
 app.use(express.json());
 
 const knex = require("knex")({
-    client: "mysql",
+    client: "sqlite",
     connection: {
         filename: path.join(__dirname, './items.db')
     },
@@ -18,6 +18,21 @@ app.get("/",(req,res)=>{
     res.sendStatus(200)
 })
 
+app.post("/purchase/:itemnumber",async(req,res)=>{
+    let itemNumber = req.params.itemnumber
+    let data = []
+    axios.get(process.env.HOST+'/getbook/itemNumber',{headers:{"itemnumber":itemNumber}})
+        .then((res)=> {
+            let itemData = res.data
+            if(itemData != {})
+            {
+                axios.put(process.env.HOST+'/updatebook/stock',data,{headers:{"opration":"decrease","itemnumber":itemNumber,"amount":1}})
+            }
+        })
+        .catch( (error) =>{
+            console.log(error);
+        })
+})
 
 app.listen(process.env.PORT, ()=>{
     console.log(`server is running on port ${process.env.PORT}`);

aa169f82660491b0d9aacfda4104c4b839d17c57
abdullah-saadeddin <46649606+abdullah-saadeddin@users.noreply.github.com>
2021-11-12 09:32:06 +0200

    implement the fronend purchase route

--- FrontendServer/app.js
+++ FrontendServer/app.js
@@ -7,15 +7,6 @@ require('dotenv').config()
 app.use(cors());
 app.use(express.json());
 
-const knex = require("knex")({
-    client: "mysql",
-    connection: {
-        filename: path.join(__dirname, './items.db')
-    },
-    useNullAsDefault: true
-});
-
-
 app.get("/",(req,res)=>{
     res.sendStatus(200)
 })
@@ -30,8 +21,15 @@ app.get("/info",(req,res)=>{
 })
 
 app.post("/purchase/:itemnumber",async(req,res)=>{
-    
-    
+    let itemNumber = req.params.itemnumber
+    let data = []
+    axios.post(process.env.ORDER_HOST+'/purchase/'+itemNumber)
+        .then((ress)=> {
+           res.sendStatus(200)
+        })
+        .catch( (error) =>{
+            console.log(error);
+        })
 })
 app.listen(process.env.PORT, ()=>{
     console.log(`server is running on port ${process.env.PORT}`);

585d17cbc1ba500ac6f3ce8646929d77cb893d35
abdullah-saadeddin <46649606+abdullah-saadeddin@users.noreply.github.com>
2021-12-22 13:47:24 +0200

    add the cashing and replica the catlog server handling

--- FrontendServer/app.js
+++ FrontendServer/app.js
@@ -2,21 +2,43 @@ const express = require('express');
 const path = require("path")
 const cors = require('cors');
 const app = express() ;
-const axios = require("axios")
+const axios = require("axios");
+let catlogIP = ["http://192.168.139.133:3003","http://192.168.139.131:3003"]
+let catCur = 0
 require('dotenv').config()
 app.use(cors());
 app.use(express.json());
 
-app.get("/",(req,res)=>{
-    res.sendStatus(200)
-})
+const collection = new Map();
+let cashMid = (req, res, next)=>{
+    
+    
+    if(collection.has(req.headers["topic"] + req.url))
+    {
+        res.send(collection.get(req.headers["topic"] + req.url)) 
+        return
+    }
+    if(collection.has(req.headers["itemnumber"] + req.url))
+    {
+        res.send(collection.get(req.headers["itemnumber"] + req.url)) 
+        return
+    }
+    else
+    {
+        next()
+    }
+
+}
 
 
-app.get("/search",(req,res)=>{
+app.get("/search",cashMid,(req,res)=>{
     let subject = req.headers["topic"]
-    axios.get(process.env.Catalog_HOST+'/getbook/subject',{headers:{subject}})
+
+    axios.get(catlogIP[catCur]+'/getbook/subject',{headers:{subject}})
         .then((ress)=> {
+            collection.set(req.headers["topic"] + req.url,ress.data)
            res.send(ress.data)
+           catCur = (catCur + 1) % catlogIP.length;
         })
         .catch( (error) =>{
             res.sendStatus(500)
@@ -28,6 +50,7 @@ app.get("/info",(req,res)=>{
     let itemnumber = req.headers["itemnumber"]
     axios.get(process.env.Catalog_HOST+'/getbook/itemNumber',{headers:{itemnumber}})
         .then((ress)=> {
+            collection.set(req.headers["itemnumber"] + req.url,ress.data[0])
            res.send(ress.data[0])
         })
         .catch( (error) =>{
@@ -41,6 +64,7 @@ app.post("/purchase/:itemnumber",async(req,res)=>{
     let data = []
     axios.post(process.env.ORDER_HOST+'/purchase/'+itemNumber)
         .then((ress)=> {
+            collection.clear()
            res.sendStatus(200)
         })
         .catch( (error) =>{

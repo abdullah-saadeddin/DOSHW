c531b8a4a500ea7d5c899754d4077fb4dc8fddf3
abdullah-saadeddin <46649606+abdullah-saadeddin@users.noreply.github.com>
2021-12-22 13:50:10 +0200

    add the catlog replica support

--- FrontendServer/app.js
+++ FrontendServer/app.js
@@ -4,7 +4,9 @@ const cors = require('cors');
 const app = express() ;
 const axios = require("axios");
 let catlogIP = ["http://192.168.139.133:3003","http://192.168.139.131:3003"]
+let orderIP = ["http://192.168.139.133:3005","http://192.168.139.131:3005"]
 let catCur = 0
+let orCur = 0
 require('dotenv').config()
 app.use(cors());
 app.use(express.json());
@@ -37,8 +39,9 @@ app.get("/search",cashMid,(req,res)=>{
     axios.get(catlogIP[catCur]+'/getbook/subject',{headers:{subject}})
         .then((ress)=> {
             collection.set(req.headers["topic"] + req.url,ress.data)
+            catCur = (catCur + 1) % catlogIP.length;
            res.send(ress.data)
-           catCur = (catCur + 1) % catlogIP.length;
+           
         })
         .catch( (error) =>{
             res.sendStatus(500)
@@ -48,9 +51,10 @@ app.get("/search",cashMid,(req,res)=>{
 
 app.get("/info",(req,res)=>{
     let itemnumber = req.headers["itemnumber"]
-    axios.get(process.env.Catalog_HOST+'/getbook/itemNumber',{headers:{itemnumber}})
+    axios.get(catlogIP[catCur]+'/getbook/itemNumber',{headers:{itemnumber}})
         .then((ress)=> {
             collection.set(req.headers["itemnumber"] + req.url,ress.data[0])
+            catCur = (catCur + 1) % catlogIP.length;
            res.send(ress.data[0])
         })
         .catch( (error) =>{
@@ -62,9 +66,10 @@ app.get("/info",(req,res)=>{
 app.post("/purchase/:itemnumber",async(req,res)=>{
     let itemNumber = req.params.itemnumber
     let data = []
-    axios.post(process.env.ORDER_HOST+'/purchase/'+itemNumber)
+    axios.post(orderIP[orCur]+'/purchase/'+itemNumber)
         .then((ress)=> {
             collection.clear()
+            orCur = (orCur + 1) % orderIP.length;
            res.sendStatus(200)
         })
         .catch( (error) =>{
